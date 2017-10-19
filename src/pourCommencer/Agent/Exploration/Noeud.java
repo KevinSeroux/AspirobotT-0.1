681e3db3ae06d74d4aa68d05bca3ca23158f7270
Chevalier-Maxime <chevalier.maxime@outlook.com>
2017-10-18 23:12:33 -0400

    Refactoring et doc de Noeud

--- src/pourCommencer/Agent/Exploration/Noeud.java
+++ src/pourCommencer/Agent/Exploration/Noeud.java
@@ -1,4 +1,5 @@
 package pourCommencer.Agent.Exploration;
+
 import pourCommencer.Agent.Action;
 import pourCommencer.Environment.EnvState;
 import pourCommencer.Environment.Position;
@@ -6,13 +7,27 @@ import pourCommencer.Environment.Position;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Classe utilisée pour les algorithmes d'explorations
+ */
 public class Noeud {
+    HashMap<Noeud, Action> successor;
+    EnvState environnement;
     private Noeud parent;
     private int pathCost;
-    HashMap<Noeud,Action> successor;
-    EnvState environnement;
     private int profondeur;
     private int performance;
+    private Position positionRobot;
+
+    public Noeud(Noeud parent, EnvState environnement, int pathCost, int profondeur, Position position, int performance) {
+        this.parent = parent;
+        this.pathCost = pathCost;
+        this.environnement = environnement;
+        this.profondeur = profondeur;
+        this.performance = performance;
+        this.successor = new HashMap<>();
+        this.positionRobot = position;
+    }
 
     public Noeud getParent() {
         return parent;
@@ -35,9 +50,8 @@ public class Noeud {
     }
 
     public void addSuccessor(Noeud n, Action a) {
-        this.successor.put(n,a);
+        this.successor.put(n, a);
     }
-
 
     public EnvState getEnvironnement() {
         return environnement;
@@ -46,7 +60,6 @@ public class Noeud {
     public void setEnvironnement(EnvState environnement) {
         this.environnement = environnement;
     }
-
 
     public int getProfondeur() {
         return profondeur;
@@ -64,34 +77,25 @@ public class Noeud {
         this.positionRobot = positionRobot;
     }
 
-    private Position positionRobot;
-
     public int getPerformance() {
         return performance;
     }
 
-    public Noeud meilleurNoeud(){
+    /**
+     * Recherche recursivement quel noeud possède la meilleure "Performance relative"
+     * @return le noeud avec la meilleure performance dans l'arbre de racine this
+     */
+    public Noeud meilleurNoeud() {
         Noeud bestFound = this;
-        for(Map.Entry<Noeud, Action> entry : successor.entrySet()) {
+        for (Map.Entry<Noeud, Action> entry : successor.entrySet()) {
             Noeud key = entry.getKey();
 
             Noeud n = key.meilleurNoeud();
-            if(bestFound.getPerformance() < n.getPerformance())
+            if (bestFound.getPerformance() < n.getPerformance())
                 bestFound = n;
         }
         return bestFound;
     }
-
-    public Noeud(Noeud parent, EnvState environnement, int pathCost, int profondeur, Position position, int performance) {
-        this.parent = parent;
-        this.pathCost = pathCost;
-        this.environnement = environnement;
-        this.profondeur = profondeur;
-        this.performance = performance;
-        this.successor = new HashMap<>();
-        this.positionRobot = position;
-    }
-
 
 
 }
